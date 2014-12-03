13d10c5eed8abf8b48767b6b86c804d81a3314e9
ryan-ju <r.ju2014@gmail.com>
2014-12-03 21:32:55 +0000

    Create first working example

---
+++ src/main/scala/com/stackoverflower/example/resteasy1/Application.scala
@@ -0,0 +1,11 @@
+package com.stackoverflower.example.resteasy1
+
+import javax.ws.rs.ApplicationPath
+
+/**
+ * Created by ryan on 03/12/14.
+ */
+@ApplicationPath("/api")
+class Application extends javax.ws.rs.core.Application {
+
+}
---
+++ src/main/scala/com/stackoverflower/example/resteasy1/resources/Health.scala
@@ -0,0 +1,17 @@
+package com.stackoverflower.example.resteasy1.resources
+
+import javax.ws.rs.core.{Response, MediaType}
+import javax.ws.rs.{Produces, Path, GET}
+
+/**
+ * Created by ryan on 01/12/14.
+ */
+@Path("/health")
+class Health {
+  @GET
+  @Path("/report")
+  @Produces(value = Array("application/xml", "application/json"))
+  def health: Response = {
+    Response.ok().build()
+  }
+}
